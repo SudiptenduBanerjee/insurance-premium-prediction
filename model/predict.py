16c0a198d6bd470851905cb025f174eb153a24ba
Sudiptendu Banerjee <sudiptendubanerjee403@gmail.com>
2025-07-13 00:49:59 +0530

    Refactor predict_output function to return predicted class and probabilities as a dictionary

--- model/predict.py
+++ model/predict.py
@@ -10,10 +10,10 @@ with open('/home/sea-dragon/insurence-premium-prediction/model/model.pkl', 'rb')
 
 MODEL_VERSION = "1.0.0"
 
+class_labels = model.classes_.tolist()
 
 def predict_output(user_input: Dict[str, Any]):
-    input_df = pd.DataFrame(user_input)
-    
-
-    output = model.predict(input_df)[0]
-    return output
+    df = pd.DataFrame([user_input])
+    predicted_class = model.predict(df)[0]
+    predicted_proba = model.predict_proba(df)[0].tolist()
+    return {"predicted_class": class_labels[predicted_class], "predicted_proba": predicted_proba}
