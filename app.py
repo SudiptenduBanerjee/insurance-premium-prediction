0c6e5966641a96db1189382934e2645280d47e7b
Sudiptendu Banerjee <sudiptendubanerjee403@gmail.com>
2025-07-13 00:31:20 +0530

    Refactor app.py by removing unused imports and commented-out code; streamline version and health check endpoints.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field, computed_field, field_validator
-from typing import Literal, Annotated
 import pickle
 import pandas as pd
+from schema.user_input import UserInput
 
 #import the ml model
 with open('/home/sea-dragon/insurence-premium-prediction/model/model.pkl', 'rb') as f:
@@ -16,10 +15,18 @@ MODEL_VERSION = "1.0.0"
 # Initialize FastAPI app
 app = FastAPI()
 
+# Define the root endpoint
+@app.get("/version")
+def get_version():
+    return {"version": MODEL_VERSION}
+
+
 @app.get("/")
 def home():
     return {"message": "Welcome to the Insurance Premium Prediction API"}
 
+
+# Health check endpoint
 @app.get("/health")
 def health_check():
     return {"message": "API is healthy"
@@ -27,70 +34,7 @@ def health_check():
             , "status": "OK"
             }   
 
-tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
-tier_2_cities = [
-    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
-    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
-    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
-    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
-    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
-    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
-]
 
-#pydantic model to validate income data
-class UserInput(BaseModel):
-    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
-    weight: Annotated[float, Field(..., gt=0, description='Weight of the user')]
-    height: Annotated[float, Field(..., gt=0, lt=2.5, description='Height of the user')]
-    income_lpa: Annotated[float, Field(..., gt=0, description='Anual salary of the user in LPA')]
-    smoker: Annotated[bool, Field(..., description='Is user a somer or not')]
-    city: Annotated[str, Field(..., description='The city where user belongs to')]
-    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
-       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
-
-    @field_validator('city')
-    @classmethod
-    def normalize_city(cls, v: str) -> str:
-        v = v.strip().title()
-        return v
-
-    @computed_field
-    @property
-    def bmi(self) -> float:
-        return self.weight/(self.height**2)
-
-    @computed_field
-    @property
-    def lifestyle_risk(self) -> str:
-        if self.smoker and self.bmi > 30:
-            return "high"
-        elif self.smoker or self.bmi > 25:
-            return "medium"
-        else:
-            return "low"
-
-    @computed_field
-    @property
-    def age_group(self) -> str:
-        if self.age < 25:
-            return "younger"
-        elif self.age < 45:
-            return "adult"
-        elif self.age < 60:
-            return "middle-aged"
-        else:
-            return "senior"
-
-    @computed_field
-    @property
-    def city_tier(self) -> int:
-        if self.city in tier_1_cities:
-            return 1
-        elif self.city in tier_2_cities:
-            return 2
-        else:
-            return 3
-    
 @app.post("/predict")
 def predict_premium(data: UserInput):
 
